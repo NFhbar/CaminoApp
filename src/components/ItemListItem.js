d719c1a67fcf387d0641e27b77c5446efb3b43a0
NFhbar <frega.nicolas@gmail.com>
2018-01-07 21:38:33 -0800

    setup format for price

--- src/components/ItemListItem.js
+++ src/components/ItemListItem.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import numeral from 'numeral';
 
 const ItemListItem = ({id, description, amount, createdAt}) => (
   <div>
     <Link to={`/edit/${id}`}>
       <h3>{description}</h3>
     </Link>
-    <p>{amount} - {createdAt}</p>
+    <p>{numeral(amount/100).format('$0,0.00')} - {createdAt}</p>
   </div>
 );
 
